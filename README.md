c740941cfeb150f3d6f6f08748df60b582a9c948
Gabriel K <word@live.com.sg>
2023-04-11 20:35:03 +0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Watchy-Shijian
+# Shijian
+A simple, no-frills Chinese watchface for [Watchy](https://watchy.sqfmi.com/). It only tells the time, so no additional settings are required.
+
+Only the files in the `src` folder are required. The `font` folder is there if you want to customise it yourself.
+
+The characters for the numeric hours are capitalised to fit with the aesthetic.
+
+Most uppercase English characters are swapped out with the numerical glyphs, so to add uppercase English characters, please convert and add your own font. See the modified glyph map in the table below:
+| Original | Replaced |
+| - | - |
+| A | 零 |
+| B | 一 |
+| C | 二 |
+| D | 三 |
+| E | 四 |
+| F | 五 |
+| G | 六 |
+| H | 七 |
+| I | 八 |
+| J | 九 |
+| K | 十 |
+| L | 壹 |
+| M | 贰 |
+| N | 叁 |
+| O | 肆 |
+| P | 伍 |
+| Q | 陆 |
+| R | 柒 |
+| S | 捌 |
+| T | 玖 |
+| U | 拾 |
+| V | 时 |
+| W | 分 |
+
+Special thanks to [Pconti31](https://github.com/Pconti31) for his [TTF2GFX](https://github.com/Pconti31/TTF2GFX) library, although I found it easier to replace in bulk using [freetype-py](https://pypi.org/project/freetype-py/).
---
+++ src/shijian.ino
@@ -0,0 +1,108 @@
+#include <Watchy.h>  //include the Watchy library
+#include "SuXinPoemSeal24pt7b.h"
+#include "HanDan24pt7b.h"
+
+#define RECT_TOP_PX 40
+#define RECT_LEFT_PX 0
+#define RECT_RADIUS 5
+
+
+class Shijian : public Watchy {  //inherit and extend Watchy class
+  using Watchy::Watchy;
+public:
+	
+	void drawWatchFace() { //override this method to customize how the watch face looks
+		uint16_t lines = 1;
+		// const String lows[10] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+		// const String teensone[11] = { "","ten", "eleven", "twelve", "thir", "four", "fif", "six", "seven", "eight", "nine" };
+		// const String teenstwo[11] = { "", "", "", "teen", "teen", "teen", "teen", "teen", "teen", "teen", "teen" };
+		// const String tens[10] = { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+		const String lclows[10] = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+		const String lcteensone[11] = { "", "K", "K", "K", "K", "K", "K", "K", "K", "K", "K" };
+		const String lcteenstwo[11] = { "", "B", "C", "D", "E", "F", "G", "H", "I", "J", "" };
+		const String lctens[10] = { "A", "K", "CK", "DK", "EK", "FK", "GK", "HK", "IK", "JK" };
+
+		const String uclows[10] = { "A", "L", "M", "N", "O", "P", "Q", "R", "S", "T" };
+		const String ucteensone[11] = { "", "U", "U", "U", "U", "U", "U", "U", "U", "U", "U" };
+		const String ucteenstwo[11] = { "", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "" };
+		const String uctens[10] = { "A", "U", "MU", "NU", "OU", "PU", "QU", "RU", "SU", "TU" };
+
+		//drawbg
+		display.fillScreen(GxEPD_WHITE);
+		display.setTextColor(GxEPD_WHITE);
+		display.fillRoundRect(RECT_LEFT_PX, RECT_TOP_PX, 200 - 2 * RECT_LEFT_PX, 200 - 2 * RECT_TOP_PX, RECT_RADIUS, GxEPD_BLACK);
+		//display.setTextWrap(false);
+
+		String hourStr, minuteStr;
+		int16_t  x1, y1;
+		uint16_t w, h;
+
+		//drawtime
+		lines += 1;
+		// display.setCursor(5, lines * 47 - 2);
+		display.setFont(&SuXinPoemSeal24pt7b);
+		if (currentTime.Hour == 0) {
+			hourStr = uctens[2] + uclows[4];
+		}
+		else if (currentTime.Hour < 10) {
+			hourStr = uclows[currentTime.Hour];
+		}
+		else if (currentTime.Hour < 20) {
+			hourStr = ucteensone[currentTime.Hour - 9];
+			if (currentTime.Hour > 10) {
+				hourStr += ucteenstwo[currentTime.Hour % 10];
+			}
+		}
+		else {
+			hourStr = uctens[currentTime.Hour / 10];
+			if (currentTime.Hour % 10 > 0) {
+				hourStr += uclows[currentTime.Hour % 10];
+			}
+		}
+		hourStr += "V";
+		display.getTextBounds(hourStr, 0, 0, &x1, &y1, &w, &h);
+		display.setCursor(100 - w / 2, lines * 47 - 2);
+		display.print(hourStr);
+
+		lines += 1;
+		display.setCursor(5, lines * 47 - 2);
+		display.setFont(&HanDan24pt7b);
+		if (currentTime.Minute == 0) {
+			// display.print("V");
+		}
+		else {
+			if (currentTime.Minute < 10) {
+				minuteStr = "A" + lclows[currentTime.Minute];
+			}
+			else if (currentTime.Minute < 20) {
+				minuteStr = lcteensone[currentTime.Minute - 9];
+				if (currentTime.Minute > 10) {
+					minuteStr += lcteenstwo[currentTime.Minute % 10];
+				}
+			}
+			else {
+				minuteStr = lctens[currentTime.Minute / 10];
+				if (currentTime.Minute % 10 > 0) {
+					minuteStr += lclows[currentTime.Minute % 10];
+				}
+			}
+			minuteStr += "W";
+			display.getTextBounds(minuteStr, 0, 0, &x1, &y1, &w, &h);
+			display.setCursor(100 - 5 - w / 2, lines * 47 - 2);
+			display.print(minuteStr);
+		}
+	}
+};
+
+watchySettings settings{};
+
+Shijian m(settings);  //instantiate your watchface
+
+void setup() {
+  m.init();  //call init in setup
+}
+
+void loop() {
+  // this should never run, Watchy deep sleeps after init();
+}
